2e1ae2d9143490bbdbd3404628d2f24aa9402d90
rjarivi <124758732+rjarivi@users.noreply.github.com>
2025-01-02 13:44:19 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.svg', '**/*.jpeg', '**/*.jpg'],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
